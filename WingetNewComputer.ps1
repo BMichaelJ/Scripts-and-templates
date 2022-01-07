c56307b84d6d02b92329adf8807ff8993a92be28
Michael Jonsson <BMichaelJ@users.noreply.github.com>
2022-01-07 14:44:23 +0100

    Create WingetNewComputer.ps1

---
+++ WingetNewComputer.ps1
@@ -0,0 +1,32 @@
+
+#Install New apps
+$apps = @(
+    @{name = "Microsoft.AzureCLI" }, 
+    @{name = "Microsoft.PowerShell" }, 
+    @{name = "Microsoft.VisualStudioCode" }, 
+    @{name = "Microsoft.WindowsTerminal" }, 
+    @{name = "Microsoft.AzureStorageExplorer" }, 
+    @{name = "Microsoft.PowerToys" }, 
+    @{name = "Git.Git" }, 
+    @{name = "WhatsApp.WhatsApp" },
+    @{name = "Microsoft.VisualStudioCode.Insiders" },
+    @{name = "Discord.Discord" },
+    @{name = "GitHub.cli" },
+    @{name = "Spotify.Spotify" }
+);
+Foreach ($app in $apps) {
+    #check if the app is already installed
+    $listApp = winget list --exact -q $app.name
+    if (![String]::Join("", $listApp).Contains($app.name)) {
+        Write-host "Installing:" $app.name
+        if ($app.source -ne $null) {
+            winget install --exact --silent $app.name --source $app.source
+        }
+        else {
+            winget install --exact --silent $app.name 
+        }
+    }
+    else {
+        Write-host "Skipping Install of " $app.name
+    }
+}
